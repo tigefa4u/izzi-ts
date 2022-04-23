47e1d344a07d23720c7b3c8a6b95d67925e4c28c
HoaX7 <krhoax7@gmail.com>
2022-04-23 16:44:24 +0530

    hod fix

--- src/modules/commands/rpg/adventure/abilityProcs/special/index.ts
+++ src/modules/commands/rpg/adventure/abilityProcs/special/index.ts
@@ -42,6 +42,16 @@ export const harbingerOfDeath = ({
 		opponentStats.totalStats.evasionTemp = 1;
 		opponentStats.totalStats.isEvadeHit = false;
 		opponentStats.totalStats.isCriticalHit = false;
+		
+		playerStats.totalStats.isEvadeHit = false;
+		playerStats.totalStats.isEvadeHit = false;
+		playerStats.totalStats.critical = 1;
+		playerStats.totalStats.criticalDamage = 1;
+		playerStats.totalStats.criticalInc = 1;
+		playerStats.totalStats.criticalTemp = 1;
+		playerStats.totalStats.evasion = 1;
+		playerStats.totalStats.evasionInc = 1;
+		playerStats.totalStats.evasionTemp = 1;
 
 		// Nullify all effects
 		playerStats.totalStats.isStunned = false;
