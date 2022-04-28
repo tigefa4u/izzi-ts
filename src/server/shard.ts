0a3e8b9ec628fbae60f9239c2a86a26c60c96008
HoaX7 <krhoax7@gmail.com>
2022-04-28 09:46:03 +0530

    new bt opt

--- src/server/shard.ts
+++ src/server/shard.ts
@@ -29,11 +29,11 @@ if (SHARD_LIST) {
 	Object.assign(shardParams, { shardList: SHARD_LIST });
 }
 
-const manager = new ShardingManager(path.join(__dirname, "client.ts"), {
+const manager = new ShardingManager(path.join(__dirname, "client.js"), {
 	execArgv: [ "-r", "ts-node/register" ],
 	totalShards: "auto",
-	token: DISCORD_TEST_BOT,
-	// token: DISCORD_BOT_TOKEN,
+	// token: DISCORD_TEST_BOT,
+	token: DISCORD_BOT_TOKEN,
 	respawn: true
 });
 
