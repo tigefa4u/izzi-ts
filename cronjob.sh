064434bc0547d741215d7c21aa2f269686aded17
hoax <kvnrivers@gmail.com>
2022-04-29 15:28:56 +0530

    cronjob shell script

---
+++ cronjob.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd ${dirname "$0"}
+job=$2
+if [[ $1 != "-job" || -z $job ]]
+then
+    echo "invalid arguments provided"
+    echo "Valid Args"
+    echo "-job <name>   Job to execute"
+elif [[ $job == "minute" ]]
+then
+    echo "executing one minute jobs ###"
+    npx ts-node -r dotenv/config src/server/pipes/cronjobs/oneMinuteTimers.ts
+    echo "one minute jobs completed.."
+elif [[ $job == "4minute" ]]
+then
+    echo "executing four minute jobs ###"
+    npx ts-node -r dotenv/config src/server/pipes/cronjobs/fourMinuteTimers.ts
+    echo "four minute jobs completed.."
+elif [[ $job == "hour" ]]
+then
+    echo "executing hourly jobs ###"
+    npx ts-node -r dotenv/config src/server/pipes/cronjobs/hourlyTimers.ts
+    echo "one hour jobs completed.."
+elif [[ $job == "premium" ]]
+then
+    echo "executing premium jobs ###"
+    npx ts-node -r dotenv/config src/server/pipes/cronjobs/premiumTier.ts
+    echo "premium jobs completed.."
+elif [[ $job == "rp" ]]
+then
+    echo "executing raid permit jobs ###"
+    npx ts-node -r dotenv/config src/server/pipes/cronjobs/raidPermitRefill.ts
+    echo "raid permit refill jobs completed.."
+elif [[ $job == "rpp" ]]
+then
+    echo "executing raid premium permit jobs ###"
+    npx ts-node -r dotenv/config src/server/pipes/cronjobs/raidPermitPremiumRefill.ts
+    echo "raid premium permit refill jobs completed.."
+fi
+
