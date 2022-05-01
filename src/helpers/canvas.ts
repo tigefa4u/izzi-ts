30fd48cda8a291662377ea2a1796bffd20a350ff
hoax <kvnrivers@gmail.com>
2022-05-01 14:56:28 +0530

    canvas creation moved to global

--- src/helpers/canvas.ts
+++ src/helpers/canvas.ts
@@ -13,6 +13,7 @@ export const createSingleCanvas: (
 ) => Promise<Canvas | undefined> = async function (card, isNotStar = false) {
 	try {
 		const ctx = canvas.getContext("2d");
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		// ctx.fillStyle = "#000000";
 		// ctx.fillRect(0, 0, canvas.width, canvas.height);
 		const image = await loadImage(card.filepath);
