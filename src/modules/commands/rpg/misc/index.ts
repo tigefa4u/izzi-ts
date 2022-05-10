6df83399bd2ab6775bb0eda5207b67ba457ce7c5
hoax <kvnrivers@gmail.com>
2022-05-10 16:11:47 +0530

    lotto

--- src/modules/commands/rpg/misc/index.ts
+++ src/modules/commands/rpg/misc/index.ts
@@ -58,6 +58,10 @@ export const lottery = async ({ context, client, options }: BaseProps) => {
 		];
 		const randomReward = randomElementFromArray(rewards);
 		if (!randomReward) return;
+		if (user.mana > user.max_mana && randomReward.key === "mana") {
+			context.channel?.sendMessage("You already have max mana you can hold");
+			return;
+		}
 		Object.assign(user, {
 			[randomReward.key]:
         (user[randomReward.key as keyof UserProps] as number) +
@@ -75,15 +79,12 @@ export const lottery = async ({ context, client, options }: BaseProps) => {
         `\nYou have leveled up! You are now level __${updatedUser.level}__. ` +
         `We have also refilled your mana __${user.max_mana}__ -> __${user.max_mana + 2}__`;
 		}
-		const updateObj = { gold: user.gold } as any;
+		const updateObj = { gold: user.gold };
 		if (randomReward.key === "exp") {
 			Object.assign(updateObj, { exp: user.exp });
 		}
 		if (randomReward.key === "mana" || randomReward.key === "gold") {
 			Object.assign(updateObj, { [randomReward.key]: user[randomReward.key] });
-			if (updateObj.mana > user.max_mana) {
-				delete updateObj.mana;
-			}
 		}
 		await Promise.all([
 			updateRPGUser({ user_tag: user.user_tag }, updateObj),
