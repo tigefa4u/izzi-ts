31c5a248b3b1c9ae0ec179e97aad398c2b437f3e
HoaX7 <krhoax7@gmail.com>
2022-08-19 02:32:13 +0530

    cronjob fix

--- src/server/pipes/cronjobs/index.ts
+++ src/server/pipes/cronjobs/index.ts
@@ -10,7 +10,7 @@ export async function init(params: {
 	await knex.raw(
 		`update users set raid_pass = raid_pass + 1, raid_permit_refilled_at = now() 
         where is_banned = false and is_premium = ${params.is_premium} ${
-	params.is_premium ? "and" : "or"
+	params.is_premium ? "or" : "and"
 } is_mini_premium = ${params.is_mini_premium} 
 		and raid_pass < max_raid_pass`
 	);
