793dbd122ff32490596f5d6fbe729d6b5e07a612
hoax <kvnrivers@gmail.com>
2022-08-21 12:36:53 +0530

    slash commands

--- src/commands/slashCommands.ts
+++ src/commands/slashCommands.ts
@@ -3,42 +3,50 @@ import { Client, Constants, GuildApplicationCommandManager } from "discord.js";
 import loggers from "loggers";
 
 export const registerSlashCommands = async (client: Client) => {
-	// const guildId = "784087004806774815";
+	const guildId = "784087004806774815";
 
-	// const guild = client.guilds.cache.get(guildId);
+	const guild = client.guilds.cache.get(guildId);
 	// if (guild) {
-	// commands = guild.commands;
+	const commands = guild?.commands;
 	// } else {
-	const commands = client?.application?.commands;
+	// const commands = client?.application?.commands;
 	// }
-	// commands?.create({
-	// 	name: "ping",
-	// 	description: "pings the bot"
-	// });
-	const allCommands = await getAllCommands();
-	if (!allCommands) return;
-	allCommands.forEach(async (command) => {
-		try {
-			await commands
-				?.create({
-					name: command.name,
-					description: `use /help or @izzi help ${command.name} for more info`,
-					options: [
-						{
-							name: "options",
-							description: `use /help or @izzi help ${command.name} for more info`,
-							type: Constants.ApplicationCommandOptionTypes.STRING,
-						},
-					],
-				});
-		} catch (err) {
-			loggers.error(
-				"Slash Commands registration failed for: " +
-		command.name,
-				err
-			);
-		}
+	commands?.create({
+		name: "iz",
+		description: "use /iz <commands>, use /help for a list of all commands",
+		options: [
+			{
+				name: "options",
+				description: "use <commands> to invoke izzi",
+				type: Constants.ApplicationCommandOptionTypes.STRING
+			}
+		]
 	});
-	console.log(`registered ${allCommands.length} slash commands`);
+	console.log("registered slash command");
+	// const allCommands = await getAllCommands();
+	// if (!allCommands) return;
+	// allCommands.forEach(async (command) => {
+	// 	try {
+	// 		await commands
+	// 			?.create({
+	// 				name: command.name,
+	// 				description: `use /help or @izzi help ${command.name} for more info`,
+	// 				options: [
+	// 					{
+	// 						name: "options",
+	// 						description: `use /help or @izzi help ${command.name} for more info`,
+	// 						type: Constants.ApplicationCommandOptionTypes.STRING,
+	// 					},
+	// 				],
+	// 			});
+	// 	} catch (err) {
+	// 		loggers.error(
+	// 			"Slash Commands registration failed for: " +
+	// 	command.name,
+	// 			err
+	// 		);
+	// 	}
+	// });
+	// console.log(`registered ${allCommands.length} slash commands`);
 	return;
 };
--- src/modules/events/interaction/index.ts
+++ src/modules/events/interaction/index.ts
@@ -11,17 +11,20 @@ const handleCommandInteraction = async (client: Client, context: CommandInteract
 		await context.deferReply({ ephemeral: true });
 		const options = context.options;
 		const content = options.getString("options");
-		let args = [ "iz", ...(content || "").split(/\s+/) ];
-		const command = await getCommand(context.commandName);
-		await context.editReply({ content: "." });
+		// let args = [ "iz", ...(content || "").split(/\s+/) ];
+		const args = (content || "").split(/\s+/);
+		const commandName = args[0];
+		if (!commandName) return;
+		const command = await getCommand(commandName);
+		await context.editReply({ content: "replied" });
 		if (!command) return;
 		if (
 			typeof commandCategory[command?.type as keyof CommandCategoryProps] !== "function"
 		)
 			return;
-		if (command.name === "guild") {
-			args = [ "iz", ...(content || "").split(/\s+/) ];
-		}
+		// if (command.name === "guild") {
+			// args = [ "iz", ...(content || "").split(/\s+/) ];
+		// }
 		const isValid = await checkUserBanned(context, client, context.user, command.name);
 		if (!isValid) return;
 		commandCategory[command?.type as keyof CommandCategoryProps]({
