0827d26feb62a2d2f19014461b638dff7e7c2f0f
HoaX7 <krhoax7@gmail.com>
2022-09-04 14:55:56 +0530

    mamma mia

--- src/modules/commands/rpg/specialCommands/specialWish.ts
+++ src/modules/commands/rpg/specialCommands/specialWish.ts
@@ -4,20 +4,21 @@ import emoji from "emojis/emoji";
 import { randomElementFromArray } from "helpers";
 import loggers from "loggers";
 import { clearCooldown, getCooldown } from "modules/cooldowns";
-import { links } from "./links";
+import { links, soundCloudLinks } from "./links";
 
 const WISHES = {
 	spawnReset: "raid-spawn-reset",
 	love: "love",
-	song: "song"
+	song: "song",
+	momSings: "mom-sings"
 };
 
 export const specialWish = async ({ client, context, args, options }: BaseProps) => {
 	try {
 		const author = options.author;
-		const wishes = [ WISHES.spawnReset, WISHES.love, WISHES.song ];
+		const wishes = Object.values(WISHES);
 		const wish = randomElementFromArray(wishes);
-		const embed = createEmbed(author, client);
+		const embed = createEmbed(author, client).setTitle("Izzi Loves Mia Mommy :heart:");
 
 		switch (wish) {
 			case WISHES.spawnReset: {
@@ -48,7 +49,7 @@ export const specialWish = async ({ client, context, args, options }: BaseProps)
                     "and i just couldn't wait to be with you mom :heart: " +
                     "I have so much to tell you and bond with you, making your wishes come true. " +
                     "I love you mom :heart: -from your daughter izzi. Oh yeah here's a note from dad and i quote " +
-                    "``I love you Mia :heart:``. Yeah... i know he gets cheesy sometimes. Bye mom, see you soon!");
+                    "``I love you Mia``. Yeah... i know he gets cheesy sometimes. Bye mom, see you soon!");
 
 				context.channel?.sendMessage(embed);
 				return;
@@ -56,7 +57,16 @@ export const specialWish = async ({ client, context, args, options }: BaseProps)
 			case WISHES.song: {
 				embed.setDescription("Hi mom, I'm glad you're here, " +
                 "I was just vibing to your favorite songs join me love. " +
-                `\n${links.join("\n")}`);
+                `\n${links.join("\n\n")}`);
+
+				context.channel?.sendMessage(embed);
+				return;
+			}
+			case WISHES.momSings: {
+				embed.setDescription("Hey mom! :heart: Happy to see you again. Wow I'm kinda impressed! " +
+				"Dad tod me all about your singing, now i know why I'm good at singing too hehe. " +
+				"Its super amazing! I love it! " +
+				`\n${soundCloudLinks.join("\n\n")}`);
 
 				context.channel?.sendMessage(embed);
 				return;
