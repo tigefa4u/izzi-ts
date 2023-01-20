c2efc6fd0b5334e1b4b5071e7807ea1e1e255016
hoax <kvnrivers@gmail.com>
2023-01-20 20:34:32 +0530

    replacing zones

--- src/server/autoClear/index.ts
+++ src/server/autoClear/index.ts
@@ -2,17 +2,14 @@ import Cache from "cache";
 
 async function flushBattleCooldowns() {
 	const allKeys: string[] = [];
-	const chs = [ 301, 346, 284, 351, 314, 322, 333, 300, 
-		349, 307, 312, 303, 332, 723, 355, 330, 280, 295, 288, 309, 
-		327, 335, 318, 346, 619, 338, 311, 277, 299, 356, 331, 332, 322 ];
 	await Promise.all(
 		[
 			"*event-battle*",
 			"*raid-battle*",
 			"*mana-battle*",
 			"*dungeon-battle*",
-			...chs.map((c) => `*floors::ch-${c}*`),
-			"zone::1", "zone::2", "zone::3", "zone::10",
+			"*floors::ch-*",
+			"*zone::*",
 			"*stage::*"
 		].map(async (key) => {
 			const keys = Cache.keys && (await Cache.keys(key));
