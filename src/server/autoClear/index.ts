d7b5f003110f40fc1867f30ee7de698aadd8e846
hoax <kvnrivers@gmail.com>
2023-01-20 18:37:57 +0530

    replacing zones

--- src/server/autoClear/index.ts
+++ src/server/autoClear/index.ts
@@ -2,10 +2,22 @@ import Cache from "cache";
 
 async function flushBattleCooldowns() {
 	const allKeys: string[] = [];
-	await Promise.all([ "*event-battle*", "*raid-battle*", "*mana-battle*", "*dungeon-battle*" ].map(async (key) => {
-		const keys = Cache.keys && await Cache.keys(key);
-		return keys || "";
-	})).then(res => allKeys.push(...res.flat()));
+	const chs = [ 301, 346, 284, 351, 314, 322, 333, 300, 
+		349, 307, 312, 303, 332, 723, 355, 330, 280, 295, 288, 309, 
+		327, 335, 318, 346, 619, 338, 311, 277, 299, 356, 331, 332, 322 ];
+	await Promise.all(
+		[
+			"*event-battle*",
+			"*raid-battle*",
+			"*mana-battle*",
+			"*dungeon-battle*",
+			...chs.map((c) => `*floors::ch-${c}*`),
+			"zone::1", "zone::2", "zone::3", "zone::10"
+		].map(async (key) => {
+			const keys = Cache.keys && (await Cache.keys(key));
+			return keys || "";
+		})
+	).then((res) => allKeys.push(...res.flat()));
 	await Promise.all(allKeys.map(async (key) => await Cache.del(key)));
 	console.log("done--", allKeys);
 }
