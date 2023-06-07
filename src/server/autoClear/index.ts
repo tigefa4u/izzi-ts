8f4fad666b6b0e0ca4cc6c49737228bbc591c753
hoax <kvnrivers@gmail.com>
2023-06-07 17:56:24 +0530

    wb battle auto clean

--- src/server/autoClear/index.ts
+++ src/server/autoClear/index.ts
@@ -12,7 +12,6 @@ async function flushBattleCooldowns() {
 			// "*zone::*",
 			// "*stage::*",
 			"*in-battle*",
-			"*worldboss-attack*"
 		].map(async (key) => {
 			const keys = Cache.keys && (await Cache.keys(key));
 			return keys || "";
