b6d93daf8ac57d774257a29bdcf7c41ba59da6f8
hoax <kvnrivers@gmail.com>
2023-07-12 17:08:42 +0530

    10% cap for easy and med raids

--- src/helpers/raidConstants.ts
+++ src/helpers/raidConstants.ts
@@ -2,4 +2,4 @@
 export const LOW_LVL_RAIDS = [ "easy", "medium" ];
 
 export const HIGH_LVL_RAIDS_SINGLE_BT_CAP = 7.5;
-export const LOW_LVL_RAIDS_SINGLE_BT_CAP = 12.5;
+export const LOW_LVL_RAIDS_SINGLE_BT_CAP = 10;
