7441129a26e0fee02353b5f02ec17cc27652ddb9
hoax <kvnrivers@gmail.com>
2023-08-11 00:04:21 +0530

    testing pg connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
 import knex from "knex";
+import loggers from "loggers";
 import knexfile from "./knexfile";
 
 const env = "development";
@@ -7,15 +8,19 @@ const envConfig = knexfile[env];
 const connection = knex(envConfig);
 
 (function () {
-	connection
-		.raw("SELECT 1")
-		.then(() => {
-			console.log("PostgreSQL connected");
-		})
-		.catch((e) => {
-			console.log("PostgreSQL not connected");
-			console.error(e);
-		});
+	setInterval(() => {
+		connection
+			.raw("SELECT 1")
+			.then(() => {
+				console.log("PostgreSQL connected");
+				loggers.info("PostgreSQL connected");
+			})
+			.catch((e) => {
+				console.log("PostgreSQL not connected");
+				loggers.error("PostgreSQL connection failed", e);
+				console.error(e);
+			});
+	}, 1000 * 60 * 60);
 })();
 
 export default connection;
