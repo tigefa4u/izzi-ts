9db725016e07f9067cf79aaba5bcd3f8690d62da
hoax <kvnrivers@gmail.com>
2023-10-01 10:06:07 +0530

    raid lobby pl fix

--- src/helpers/embedLists/lobbies.ts
+++ src/helpers/embedLists/lobbies.ts
@@ -1,9 +1,13 @@
+import { OverallStatsProps } from "@customTypes";
 import { RaidProps } from "@customTypes/raids";
 import { EmbedFieldData } from "discord.js";
 import { emojiMap } from "emojis";
 import emoji from "emojis/emoji";
+import { numericWithComma } from "helpers";
 import { MAX_RAID_LOBBY_MEMBERS } from "helpers/constants";
+import { statMultiplier } from "helpers/raid";
 import { titleCase } from "title-case";
+import { clone } from "utility";
 
 export const createLobbiesList = (
 	array: RaidProps[],
@@ -12,12 +16,25 @@ export const createLobbiesList = (
 ) => {
 	const fields: EmbedFieldData[] = [];
 	array.map((item, i) => {
+		const stats = clone(item.stats);
+		let pl = stats.battle_stats.stats["strength"];
+		const keys = Object.keys(stats.battle_stats.stats);
+		keys.forEach((key) => {
+			if ([ "vitality", "dexterity", "intelligence", "defense" ].includes(key)) {
+				stats.battle_stats.stats[key as keyof OverallStatsProps] = Math.round(
+					(stats.battle_stats.stats[key as keyof OverallStatsProps] || 0) *
+			  statMultiplier[item.stats.rawDifficulty.toLowerCase()]
+				);
+	
+				pl = pl + (stats.battle_stats.stats[key as keyof OverallStatsProps] || 0);
+			}
+		});
 		fields.push({
 			name: `#${i + 1 + (currentPage - 1) * perPage} | ${item.raid_boss
 				.map((i) => `${titleCase(i.name)} ${emojiMap(i.abilityname)}`)
 				.join(", ")} [${titleCase(item.stats.difficulty)}]`,
-			value: `Level ${item.stats.battle_stats.boss_level} | ` +
-            `Power Level: ${item.stats.battle_stats.power_level}` +
+			value: `Level ${numericWithComma(item.stats.battle_stats.boss_level)} | ` +
+            `Power Level: ${numericWithComma(pl)}` +
             `${item.loot.drop.event?.shard ? `| ${item.loot.drop.event?.shard} ${emoji.shard}` : ""} ` +
             `| ID: ${item.id} **[${Object.keys(item.lobby).length} / ${MAX_RAID_LOBBY_MEMBERS}]**`,
 		});
