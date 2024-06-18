b32d7f68c30d599ade51fb31e566c0bc438f351e
hoax <kvnrivers@gmail.com>
2024-06-18 19:15:41 +0530

    update character name

--- src/modules/commands/rpg/specialCommands/subcommands.ts
+++ src/modules/commands/rpg/specialCommands/subcommands.ts
@@ -1,46 +1,66 @@
-export const subcommands = [ {
-	name: "sort",
-	alias: []
-}, {
-	name: "chl", // character_level
-	alias: []
-}, {
-	name: "chr", // character_rank
-	alias: []
-}, {
-	name: "rdmg",
-	alias: []
-}, {
-	name: "help",
-	alias: []
-}, {
-	name: "re",
-	alias: []
-}, {
-	name: "wspawn",
-	alias: []
-}, {
-	name: "wstart",
-	alias: []
-}, {
-	name: "wend",
-	alias: []
-}, {
-	name: "wdmg",
-	alias: []
-}, {
-	name: "wsend",
-	alias: []
-}, {
-	name: "force-start", // start a izzi player acc
-	alias: [ "ustart" ]
-}, {
-	name: "clearimagecache",
-	alias: [ "cc" ]
-}, {
-	name: "donation",
-	alias: [ "dono", "donations" ]
-}, {
-	name: "donated",
-	alias: []
-} ];
+export const subcommands = [
+	{
+		name: "sort",
+		alias: [],
+	},
+	{
+		name: "chl", // character_level
+		alias: [],
+	},
+	{
+		name: "chr", // character_rank
+		alias: [],
+	},
+	{
+		name: "chn", //character_name
+		alias: [],
+	},
+	{
+		name: "rdmg",
+		alias: [],
+	},
+	{
+		name: "help",
+		alias: [],
+	},
+	{
+		name: "re",
+		alias: [],
+	},
+	{
+		name: "wspawn",
+		alias: [],
+	},
+	{
+		name: "wstart",
+		alias: [],
+	},
+	{
+		name: "wend",
+		alias: [],
+	},
+	{
+		name: "wdmg",
+		alias: [],
+	},
+	{
+		name: "wsend",
+		alias: [],
+	},
+	{
+		name: "force-start", // start a izzi player acc
+		alias: [ "ustart" ],
+	},
+	{
+		name: "clearimagecache",
+		alias: [ "cc" ],
+	},
+	{
+		name: "donation",
+		alias: [ "dono", "donations" ],
+	},
+	{
+		name: "donated",
+		alias: [],
+	},
+];
